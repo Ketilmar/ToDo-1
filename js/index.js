1e59b255b798c3ce2d1bcbb528111cb2af074876
Ketil Kodehode <ketilmar.kodehode@gmail.com>
2022-06-13 14:23:45 +0200

    logikk fungerer. Må justere på css

--- js/index.js
+++ js/index.js
@@ -4,18 +4,42 @@ const inputBtn = document.querySelector("#input-btn")
 const todoList = document.querySelector(".todo-list")
 // const newItem = document.createTextNode("hei. Dette er en test")
 
+
+// listen for click on "save now" button. Then runs addTodo() function
 document.getElementById("input-btn").addEventListener("click", (e) => {
     console.log(e);
     console.log(input.value);
     // document.createElement("li").appendChild(newItem)
+
     console.log(todoList);
-    const li = document.createElement("li")
-    li.textContent = "li: test tekst"
-    console.log(li);
-    li.append(li)
+    if (!input.value){
+        end;
+    }
+    addTodo()
+    input.value = ""
 })
 
+// her lager jeg en eventlistener for å slette oppføringer
+document.getElementById()
 
+// creates 3 new elements and adds the value from input field
+function addTodo() {
+    const div = document.createElement("div")
+    const p = document.createElement("p")
+    const button = document.createElement("button");
+    let date = new Date()    
+    button.classList = "delete-btn"
+    p.textContent = input.value
+    todoList.append(div)
+    div.append(date.toLocaleDateString(), ": " , p, button)
+
+    console.log(date.toTimeString());
+}
+
+
+function deleteTodo() {
+
+}
 // input1.addEventListener("click", (inputBtn) => {
 //     console.log(inputBtn);
 // })
