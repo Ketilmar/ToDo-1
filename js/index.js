c5746101db0bb61c04287543b63946591abf90d1
Ketil Kodehode <ketilmar.kodehode@gmail.com>
2022-06-14 14:15:55 +0200

    tester ut en ny funksjon for å lage elementer

--- js/index.js
+++ js/index.js
@@ -2,7 +2,6 @@
 const input = document.querySelector("#input")
 const inputBtn = document.querySelector("#input-btn")
 const todoList = document.querySelector(".todo-list")
-// const newItem = document.createTextNode("hei. Dette er en test")
 
 // listen for click on "save now" button. Then runs addTodo() function
 document.getElementById("input-btn").addEventListener("click", (e) => {
@@ -10,17 +9,28 @@ document.getElementById("input-btn").addEventListener("click", (e) => {
     console.log(input.value);
     // document.createElement("li").appendChild(newItem)
 
-    console.log(todoList);
-    if (!input.value){
-        end;
-    }
+    // console.log(todoList);
+
+    // runs addTodo() function and clears input field
     addTodo()
     input.value = ""
 })
 
+// listen for keydown, with guard clause to return for all other keys than Enter
+document.getElementById("input").addEventListener("keydown", (e) => {
+    if(e.code !== "Enter") return;
+    // console.log(e);
+    addTodo()
+    input.value = ""
+})
 
 // creates 3 new elements and adds the value from input field
 function addTodo() {
+    // checks for actual input before creating a new todo item
+    if (!input.value){
+        return;
+    }
+
     const div = document.createElement("div")
     const p = document.createElement("p")
     const button = document.createElement("button");
@@ -29,31 +39,41 @@ function addTodo() {
     p.textContent = input.value
     todoList.append(div)
     div.append(date.toLocaleDateString(), ": " , p, button)
-
-    // console.log(date.toTimeString());
+    input.focus()
 }
 
-// her lager jeg en eventlistener for å slette oppføringer
+
+
+
+function createNewNode(nodeType, appendNodeTo, classes, text ){
+    // console.log(nodeType, appendNodeTo, classes, text);
+    const ourNode = document.createElement(nodeType)
+    // console.log(appendNodeTo);
+    // ourNode.append(appendNodeTo);
+    appendNodeTo.append(ourNode);
+    ourNode.setAttribute('class', classes);
+    ourNode.textContent = text;
+
+    console.log(ourNode);
+};
+    createNewNode("button", document.body, ".delete-btn", "Dette er en tekst")
+
+
+
+
+// eventListener for deleting todo's.
 document.addEventListener('click', (e) => {
-    let rmElement = document.getElementsByClassName(e.target.className);
-    // console.log(rmElement[rmElement.length - 2]);
-    console.log(rmElement);
-    // document.removeChild(e.path[2])
 
-    // while (rmElement[rmElement.length - 1].firstChild){
-    //     console.log("yeah");
-    //     // rmElement[rmElement.length - 1].removeChild(rmElement[rmElement.length - 1].firstChild);
-    // }
+    // guard clause to prevent from deleting everything we click except delete-btn
+    if (!e.target.classList.contains("delete-btn")) return;
+
+    // popup to prevent accidental deleting
+    if(confirm("Are u sure you want to delete?")) {
+    } 
+        else {return};
+
+    // removes the parent of the click event target
+    e.target.parentNode.remove();
+
+
 })
-
-function deleteTodo() {
-   
-}
-// input1.addEventListener("click", (inputBtn) => {
-//     console.log(inputBtn);
-// })
- 
-// function testFunc1() {
-//     console.log(input2.value);
-//     input2.value = ""
-// }
