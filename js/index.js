1103152952bc89e23ab660e9790e29afb9e11bb7
Ketil Markhus <ketil@keebit.com>
2022-06-14 08:30:39 +0200

    tester slette metoder

--- js/index.js
+++ js/index.js
@@ -4,7 +4,6 @@ const inputBtn = document.querySelector("#input-btn")
 const todoList = document.querySelector(".todo-list")
 // const newItem = document.createTextNode("hei. Dette er en test")
 
-
 // listen for click on "save now" button. Then runs addTodo() function
 document.getElementById("input-btn").addEventListener("click", (e) => {
     console.log(e);
@@ -19,8 +18,6 @@ document.getElementById("input-btn").addEventListener("click", (e) => {
     input.value = ""
 })
 
-// her lager jeg en eventlistener for å slette oppføringer
-document.getElementById()
 
 // creates 3 new elements and adds the value from input field
 function addTodo() {
@@ -33,12 +30,24 @@ function addTodo() {
     todoList.append(div)
     div.append(date.toLocaleDateString(), ": " , p, button)
 
-    console.log(date.toTimeString());
+    // console.log(date.toTimeString());
 }
 
+// her lager jeg en eventlistener for å slette oppføringer
+document.addEventListener('click', (e) => {
+    let rmElement = document.getElementsByClassName(e.target.className);
+    // console.log(rmElement[rmElement.length - 2]);
+    console.log(rmElement);
+    // document.removeChild(e.path[2])
+
+    // while (rmElement[rmElement.length - 1].firstChild){
+    //     console.log("yeah");
+    //     // rmElement[rmElement.length - 1].removeChild(rmElement[rmElement.length - 1].firstChild);
+    // }
+})
 
 function deleteTodo() {
-
+   
 }
 // input1.addEventListener("click", (inputBtn) => {
 //     console.log(inputBtn);
