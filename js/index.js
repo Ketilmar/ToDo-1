7b9e789640b5336cba4c880862a6af72137bb274
Ketil Kodehode <ketilmar.kodehode@gmail.com>
2022-06-17 12:31:56 +0200

    innlevert versjon til oppgave 'todo'

--- js/index.js
+++ js/index.js
@@ -4,42 +4,30 @@ const inputBtn = document.querySelector("#input-btn")
 const todoList = document.querySelector(".todo-list")
 
 
-// listen for click on "save now" button. Then runs addTodo() function
+
+// listen for click on "save now" button. Runs addTodo() function and clears input field
 document.getElementById("input-btn").addEventListener("click", (e) => {
-    // console.log(e);
-    // console.log(input.value);
-    // document.createElement("li").appendChild(newItem)
 
-    // console.log(todoList);
-
-    // runs addTodo() function and clears input field
     addTodo()
     input.value = ""
 })
-
 
 // listen for keydown, with guard clause to return for all other keys than Enter
 document.getElementById("input").addEventListener("keydown", (e) => {
     if(e.code !== "Enter") return;
-    // console.log(e);
+    
     addTodo()
     input.value = ""
 
-    // del av sort testing
-    // sortByDate()
-
     // testing
-    let htmlDocument = e.path[e.path.length - 2]
-    console.log(htmlDocument);
-
-    // debug 
-    console.log(todoList);
-
+    // let htmlDocument = e.path[e.path.length - 2]
 })
+
 
 
 // Creates an li node with paragraph and button childnodes. Adds the value from input field.
 function addTodo() {
+
     // checks for actual input.
     if (!input.value){
         return;
@@ -55,39 +43,13 @@ function addTodo() {
     pEl.textContent = input.value;
 
     todoList.append(liEl);
-    // liEl.append(date.toLocaleDateString(), ": " , pEl, buttonEl)
-    liEl.append(date.toLocaleTimeString(), ": " , pEl, buttonEl)
+    liEl.append(date.toLocaleString("no-NO"), ":  " , pEl, buttonEl)
 
 
     // debug
     // console.log(date.toLocaleDateString(), date.toLocaleTimeString());
-
-    // tester noe med array
-    // let todoArray = []
-    // todoArray += div.append(date.toLocaleDateString(), ": " , p, button)
-    // console.log(todoArray.length);
-    // input.focus()
 }
 
-
-
-
-// //forsøk på å teste en annen metode til å lage dynamisk innhold. Har ikke fått det helt til mtp. plassering av knapper
-// function createNewNode(nodeType, appendNodeTo, classes, text ){
-//     // console.log(nodeType, appendNodeTo, classes, text);
-//     const ourNode = document.createElement(nodeType)
-//     // console.log(appendNodeTo);
-//     // ourNode.append(appendNodeTo);
-//     ourNode.setAttribute('class', classes);
-//     ourNode.textContent = text;
-//     const div = document.createElement("div")
-//     appendNodeTo.append(div, ourNode);
-//     // todoList.append(div);
-
-//     console.log(div, ourNode);
-//     // console.log(document.body);
-// };
-// createNewNode("button", todoList, ".delete-btn", "Dette er en tekst")
 
 
 
@@ -110,8 +72,7 @@ document.addEventListener('click', (e) => {
 })
 
 
-// sort function. Sorting by text content of "todo-list > li" node that 
-// is a timestring.
+// sort function. Sorting by timestring of "todo-list > li"
 let numericalIndex = false
 
 function sortByDate() {
@@ -130,12 +91,16 @@ function sortByDate() {
     // console.log(document.querySelector(".todo-list"));
 }
 
+
+// sort alphabetic function
 let alphabeticIndex = false;
 
 function sortByName() {
     let todoArrayName = [];
+
     // add (push) each element to end of array in sequense
     document.querySelectorAll(".todo-list > li").forEach(li => todoArrayName.push(li));
+
     // console.log(todoArrayName[0].firstElementChild);
     todoArrayName.sort((a, b) => a.firstElementChild.innerText.localeCompare(b.firstElementChild.innerText));
 
@@ -144,3 +109,29 @@ function sortByName() {
 
     todoArrayName.forEach(node => document.querySelector(".todo-list").append(node));
 }
+
+
+
+
+
+//-------------------------------
+// div testing and junk
+//-------------------------------
+
+// // forsøk på å teste en annen metode til å lage dynamisk innhold. Har ikke fått det helt til mtp. plassering av knapper
+// // Kommer tilbake til dette på et senere tidspunkt
+// function createNewNode(nodeType, appendNodeTo, classes, text ){
+//     // console.log(nodeType, appendNodeTo, classes, text);
+//     const ourNode = document.createElement(nodeType)
+//     // console.log(appendNodeTo);
+//     // ourNode.append(appendNodeTo);
+//     ourNode.setAttribute('class', classes);
+//     ourNode.textContent = text;
+//     const div = document.createElement("div")
+//     appendNodeTo.append(div, ourNode);
+//     // todoList.append(div);
+
+//     console.log(div, ourNode);
+//     // console.log(document.body);
+// };
+// createNewNode("button", todoList, ".delete-btn", "Dette er en tekst")
